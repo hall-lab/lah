893a203c7679d75441d9e3a60667cf91e2083ea4
Eddie Belter <ebelter@gmail.com>
2019-10-09 17:29:24 -0500

    SX CLI

---
+++ tests/cli_test.py
@@ -0,0 +1,19 @@
+import subprocess, tempfile, unittest
+
+class SxCliTest(unittest.TestCase):
+    def setUp(self):
+        self.out = tempfile.TemporaryFile()
+
+    def tearDown(self):
+        self.out.close()
+
+    def test1_sx(self):
+        rv = subprocess.call(['sx'], stdout=self.out)
+        self.assertEqual(rv, 0)
+
+# -- SxCliTest
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+#-- __main__
