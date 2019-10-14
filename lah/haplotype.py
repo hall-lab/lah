b6b1ff344678506b181bd002891723647ada348e
Eddie Belter <ebelter@gmail.com>
2019-10-14 17:44:05 -0500

    Haplotype
    separate __init__

--- lah/haplotype.py
+++ lah/haplotype.py
@@ -1,22 +1,28 @@
-
 class Haplotype():
-    def __init__(self, edges):
+    def __init__(self, id, chr, start, stop, rids):
+        self.id = id
+        self.chr = chr
+        self.start = start
+        self.stop = stop
+        self.length = self.stop - self.start
+        self.rids = rids
+
+    def from_edges(edges):
         hids = set()
-        self.chr = edges[0].start[0]
-        self.start = edges[0].start[1]
-        self.stop = edges[0].stop[1]
-        self.rids = set()
+        chr = edges[0].start[0]
+        start = edges[0].start[1]
+        stop = edges[0].stop[1]
+        rids = set()
         for edge in edges:
             hids.add(edge.hid)
-            self.rids.add(edge.rid)
-            if edge.start[1] < self.start:
-                self.start = edge.start[1]
-            if edge.stop[1] > self.stop:
-                self.stop = edge.stop[1]
+            rids.add(edge.rid)
+            if edge.start[1] < start:
+                start = edge.start[1]
+            if edge.stop[1] > stop:
+                stop = edge.stop[1]
         if len(hids) != 1:
             raise Exception("Multiple haplotypes in edges! {}".format(hids))
-        self.id = hids.pop()
-        self.length = self.stop - self.start
+        return Haplotype(id=hids.pop(), chr=chr, start=start, stop=stop, rids=rids)
 
     def reads(self):
         rds = list(self.rids)
--- tests/lah_haplotype_test.py
+++ tests/lah_haplotype_test.py
@@ -4,22 +4,32 @@ from .context import lah
 import lah.edge_map, lah.haplotype
 
 class LahHaplotypeTest(unittest.TestCase):
+    def setUp(self):
+        self.id = "3"
+        self.chr = "chr1"
+        self.start = 19338752
+        self.stop = 19391199
+        self.rids = ['m54238_180914_183539/11207193/ccs', 'm54238_180914_183539/11665587/ccs', 'm54238_180914_183539/17171007/ccs']
 
     def test1_haplotype(self):
+        haplotype = lah.haplotype.Haplotype(id=self.id, chr=self.chr, start=self.start, stop=self.stop, rids=self.rids)
+        self.assertIsNotNone(haplotype)
+
+    def test2_haplotype_from_edges(self):
         edges = []
         edge_map_fn = os.path.join(os.path.dirname(__file__), "data", "lah_haplotype", "hap3.edge-map.tsv")
         with open(edge_map_fn, "r") as f:
             for line in f.readlines():
                 line = line.rstrip()
                 edges.append(lah.edge_map.parse_edge_map(line))
-        hap = lah.haplotype.Haplotype(edges=edges)
-        self.assertEqual(hap.id, "3")
-        rds = ['m54238_180914_183539/11207193/ccs', 'm54238_180914_183539/11665587/ccs', 'm54238_180914_183539/17171007/ccs']
-        self.assertEqual(hap.reads(), rds)
-        self.assertEqual(hap.chr, "chr1")
-        self.assertEqual(hap.start, 19338752)
-        self.assertEqual(hap.stop, 19391199)
-        self.assertEqual(hap.length, 52447)
+        hap = lah.haplotype.Haplotype.from_edges(edges=edges)
+
+        self.assertEqual(hap.id, self.id)
+        self.assertEqual(hap.chr, self.chr)
+        self.assertEqual(hap.start, self.start)
+        self.assertEqual(hap.stop, self.stop)
+        self.assertEqual(hap.length, (self.stop - self.start))
+        self.assertEqual(hap.reads(), self.rids)
 
 # -- LahHaplotypeTest
 
