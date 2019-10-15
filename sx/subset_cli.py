536d00a8525f2f81daaa3c47776e52fc359318e3
Eddie Belter <ebelter@gmail.com>
2019-10-15 10:51:18 -0500

    SX subset actual code

--- sx/subset_cli.py
+++ sx/subset_cli.py
@@ -1,5 +1,7 @@
 import click, os, socket, sys
 
+import sx.subset
+
 # SUBSET
 # - by-name
 
@@ -14,9 +16,12 @@ def sx_subset_cli():
 @click.argument("input", type=click.STRING)
 @click.argument("output", type=click.STRING)
 @click.option("--reads", type=click.STRING, help="File of read names to subset by.")
-def sx_subset_by_name_cli():
+def sx_subset_by_name_cli(input, output, reads):
     """
     Subset sequences by names.
     """
-    subset.by_name(input=input, output=output, reads_fn=reads)
+    # FIXME input/outputs
+    output_f = open(output, "a+")
+    sx.subset.by_name(input=input, output=output_f, reads_fn=reads)
+    output_f.close()
 sx_subset_cli.add_command(sx_subset_by_name_cli, name="by-name")
