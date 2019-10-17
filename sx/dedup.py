a7d984010d44a3ef0b039392aad6f123113d0aea
Eddie Belter <ebelter@gmail.com>
2019-10-17 16:31:49 -0500

    Use SxReader in dedup

--- sx/dedup.py
+++ sx/dedup.py
@@ -1,13 +1,12 @@
 import os, sys
-
 from Bio import SeqIO
+import sx.io
 
 def by_name(input, output):
     seen = set()
-    with open(input, "r") as input_f:
-        for seq in SeqIO.parse(input_f, "fastq"):
-            if not seq.id in seen:
-                SeqIO.write(seq, output, "fastq")
-                seen.add(seq.id)
+    for seq in sx.io.SxReader(input):
+        if not seq.id in seen:
+            SeqIO.write(seq, output, "fastq")
+            seen.add(seq.id)
 
 #-- by_name
