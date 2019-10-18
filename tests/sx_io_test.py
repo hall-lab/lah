ff3ac96b91b2d962797b7a9dfca7da9ae42472df
Eddie Belter <ebelter@gmail.com>
2019-10-18 13:12:28 -0500

    SX IO reader tests

--- tests/sx_io_test.py
+++ tests/sx_io_test.py
@@ -16,14 +16,20 @@ class SxIoTest(unittest.TestCase):
             sx.io.type_for_file("blah")
         with self.assertRaisesRegex(Exception, "Unknown sequence file extension: \.blah"):
             sx.io.type_for_file("file.blah")
+        for ext in (".fastq", ".fq"):
+            self.assertEqual(sx.io.type_for_file("file"+ext), "fastq")
+        for ext in (".fasta", ".fna", ".fa"):
+            self.assertEqual(sx.io.type_for_file("file"+ext), "fasta")
 
     def test2_reader(self):
         self.assertTrue(True)
-        #i = os.path.join(self.data_d, "by-name.in.fastq")
-        #sx.dedup.by_name(input=i, output=self.out)
-        #expected_fn = os.path.join(self.data_d, "by-name.out.fastq")
-        #self.out.flush()
-        #self.assertTrue(filecmp.cmp(self.out.name, expected_fn))
+        in_fastq_fn = os.path.join(os.path.dirname(__file__), "data", "io", "in.fastq")
+        reader = sx.io.SxReader(seq_fn=in_fastq_fn)
+        self.assertEqual(reader.seq_type, "fastq")
+        cnt = 0
+        for seq in reader:
+            cnt += 1
+        self.assertEqual(cnt, 4)
 
 # -- SxIoTest
 
