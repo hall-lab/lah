3e5f3a53a738dd7d4a96da28e94a6544b1ab07a7
Eddie Belter <ebelter@gmail.com>
2019-11-18 19:44:40 -0600

    DB Ingest
    add metrics
    add counter
    commit and get new session after 2k haps

--- lah/db_ingest_cmd.py
+++ lah/db_ingest_cmd.py
@@ -42,10 +42,18 @@ def db_ingest_cmd(chromosome_name, dbfile, haplotigs_fn, headers):
 
     print("Ingesting haplotigs...")
     haplotig_iter = HaplotigIterator(in_fn=haplotigs_fn, headers=headers)
+    metrics = { "total": 0, "reads": 0 }
     for raw in haplotig_iter:
         haplotig = Haplotig(name=raw["hid"], chromosome_id=chromosome.id, file_pos=raw["file_pos"], read_cnt=len(raw["rids"]))
         session.add(haplotig)
+        metrics["total"] += 1
+        metrics["reads"] += haplotig.read_cnt
+        if metrics["total"] % 2000 == 0:
+            print(" {} ...".format(metrics["total"]))
+            session.commit()
+            session = sessionmaker()
     session.commit()
 
-    # TODO haplotigs stats
+    print("Haplotigs: {}".format(metrics["total"]))
+    print("Reads: {}".format(metrics["reads"]))
     print("Ingest chromosome ... DONE")
