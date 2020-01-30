ca38d21f0243dc9bd6cdefeb1d8becf15aa03f9a
Eddie Belter <ebelter@gmail.com>
2020-01-30 11:53:19 -0600

    Use iterator to get unbinned reads

--- lah/unbinned.py
+++ lah/unbinned.py
@@ -2,22 +2,31 @@ from lah.db import LahDb
 from lah.haplotig import Haplotig
 from lah.seqfiles import Seqfile
 
-def read_names():
-    read_names = set()
+def unbinned_read_idxs():
     session = LahDb.session()
-    for seqfile in session.query(Seqfile).all():
-        idx_fn = ".".join([seqfile.fn, "fai"])
-        with open(idx_fn, "r") as idx:
-            for line in idx.readlines():
-                read_name = line.split("\t")[0]
-                read_names.add(read_name)
 
+    binned_read_names = set()
     for haplotig in session.query(Haplotig).all():
         haplotig.load_reads()
-        for read_name in haplotig.reads:
-            if read_name in read_names:
-                read_names.remove(read_name)
+        binned_read_names.update(haplotig.reads)
 
+    for seqfile in session.query(Seqfile).all():
+        idx_fn = ".".join([seqfile.fn, "fai"])
+        with open(idx_fn, "r") as idx_f:
+            for line in idx_f:
+                idx = line.rstrip().split("\t")
+                if idx[0] in binned_read_names:
+                    binned_read_names.remove(idx[0])
+                else:
+                    yield idx
+
+    session.close()
+
+def read_names():
+    unbinned = unbinned_read_idxs()
+    read_names = set()
+    for idx in unbinned:
+        read_names.add(idx[0])
     return read_names
 
 #-- read_names
