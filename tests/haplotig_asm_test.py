5b9aaa3c13d4a4102d50afb9fa731273484a90be
Eddie Belter <ebelter@gmail.com>
2020-02-27 13:18:09 -0600

    Haplotig ASM test uses dataset

--- tests/haplotig_asm_test.py
+++ tests/haplotig_asm_test.py
@@ -1,47 +1,17 @@
-import filecmp, os, shutil, subprocess, tempfile, unittest
+import filecmp, os, subprocess, unittest
 from mock import patch
 from click.testing import CliRunner
 
+from tests.dataset import Dataset
 from lah.db import LahDb
 from lah.models import *
 from lah.cli import cli
 from lah.haplotig_asm import haplotig_asm_cmd as cmd
-from lah.db_cli import db_create_cmd
 
 class HaplotigAsmTest(unittest.TestCase):
     def setUp(self):
-        self.data_d = os.path.join(os.path.dirname(__file__), "data", "haplotig-asm2")
-        self.temp_d = tempfile.TemporaryDirectory()
-        self.temp_dn = self.temp_d.name
-        self.dbfile = os.path.join(self.temp_dn, "test.db")
+        self.dataset = Dataset()
         self.haplotig_name = "402_0_1_0"
-        shutil.copy(os.path.join(self.data_d, "haplotigs.tsv"), self.temp_dn)
-
-        runner = CliRunner()
-        result = runner.invoke(db_create_cmd, [self.dbfile])
-        try:
-            self.assertEqual(result.exit_code, 0)
-        except:
-            print(result.output)
-            raise
-
-        result = runner.invoke(cli, ["-d", self.dbfile, "db", "ingest", "-f", os.path.join(self.temp_dn, "haplotigs.tsv"), "-g", "NA,rid,hid"])
-        try:
-            self.assertEqual(result.exit_code, 0)
-        except:
-            print(result.output)
-            raise
-
-        db = LahDb(dbfile=self.dbfile)
-        db.connect()
-        session = db.session()
-        haplotig = session.query(Haplotig).filter_by(name=self.haplotig_name).one()
-        self.haplotig_seqfile_fn = haplotig.seqfile_fn(self.temp_dn)
-        self.haplotig_asm_fn = os.path.join(self.temp_dn, haplotig.asm_bn())
-        session.close()
-
-    def tearDown(self):
-        self.temp_d.cleanup()
 
     @patch("tempfile.TemporaryDirectory")
     @patch("subprocess.check_call")
@@ -56,19 +26,29 @@ class HaplotigAsmTest(unittest.TestCase):
 
         # Overwirte the subprocess check_call and tempfile tempd
         check_call_patch.return_value = 0
-        tempd_patch.return_value = self.temp_d
+        tempd_patch.return_value = self.dataset.temp_d
+
+        db = LahDb(dbfile=self.dataset.dbfile)
+        db.connect()
+        session = db.session()
+        haplotig = session.query(Haplotig).filter_by(name=self.haplotig_name).one()
 
         # Haplotig seqfile (reads)
-        with open(self.haplotig_seqfile_fn, "w") as f:
+        haplotig_seqfile_fn = haplotig.seqfile_fn(self.dataset.dn)
+        with open(haplotig_seqfile_fn, "w") as f:
             f.write("HAPLOTIG READS FASTQ\n")
 
         # Haplotig ctgs fasta (output)
-        with open(self.haplotig_asm_fn, "w") as f:
+        haplotig_asm_fn = os.path.join(self.dataset.dn, haplotig.asm_bn())
+        with open(haplotig_asm_fn, "w") as f:
             f.write("HAPLOTIG ASSEMBLY FASTA\n")
+
+        session.close()
+        db.disconnect()
 
         # Run the command
         #LahDb.__current = None
-        result = runner.invoke(cli, ["-d", self.dbfile, "haplotig", "asm", self.haplotig_name, "--retain-files"])
+        result = runner.invoke(cli, ["-d", self.dataset.dbfile, "haplotig", "asm", self.haplotig_name, "--retain-files"])
         try:
             self.assertEqual(result.exit_code, 0)
         except:
